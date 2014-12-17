b0dbf8f64d9112a121d636b140477e336c4b5730
Michael Doliner <msd83@cornell.edu>
2014-12-17 14:59:28 -0500

    Initializes project.

---
+++ js/snake.js
@@ -0,0 +1,11 @@
+(function () {
+
+  if (typeof SnakeGame === "undefined") {
+    window.SnakeGame = {};
+  }
+
+  var Snake = SnakeGame.Snake = function () {
+    this.segments = [[9,9]];
+  }
+
+})();
